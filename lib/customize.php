214de6201684d1dd0cb96d806b0e70c5e4a2b8aa
Jarldude <grumpy.jarl@gmail.com>
2022-01-09 10:05:26 +0200

    color picked added

--- lib/customize.php
+++ lib/customize.php
@@ -2,7 +2,55 @@
 
     function _themaname_customize_register( $wp_customize ){
 
+
+        /*################################# PARTIAL REFRESH SETTINGS ###############################*/
         $wp_customize->get_setting('blogname')->transport = 'postMessage';
+
+        $wp_customize->selective_refresh->add_partial('blogname', array(
+            'selector' => '.c-header__blogname',
+            'container_inclusive'=> false,
+            'render_callback' => function(){
+                bloginfo('name');
+            }
+        ));
+
+        $wp_customize->selective_refresh->add_partial('_themename_footer_partial', array(
+            'settings' => array('_themename_footer_bg', '_themename_footer_layout'),
+            'selector' => '#footer',
+            'container_inclusive'=> false,
+            'render_callback' => function(){
+                get_template_part( 'template-parts/footer/widgets' );
+                get_template_part( 'template-parts/footer/info' );
+            }
+        ));
+
+
+
+
+
+        /*################################# GENARAL SETTINGS ###############################*/
+        $wp_customize->add_section('_themename_general_options', array(
+            'title' =>  esc_html__( 'General Theme Options', '_themename'),
+            'description' => esc_html__( 'You can change General theme options here.', '_themename'),
+        ));
+
+        $wp_customize->add_setting('_themename_accent_colour', array(
+            'default' => '#20ddae',
+            'transport' => 'postMessage',
+            'sanitize_callback' => 'sanitize_hex_color'
+            
+        ));
+
+        $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, '_themename_accent_colour',
+        array(
+            'label'=>__('Accent Colour', '_themename'),
+            'section' => '_themename_general_options'
+        )));
+
+
+
+
+        /*################################# FOOTER SETTINGS ###############################*/
 
         $wp_customize->add_section('_themename_footer_options', array(
             'title' =>  esc_html__( 'Footer Options', '_themename'),
@@ -21,7 +69,44 @@
             'label' => esc_html__( 'Site info', '_themename'),
             'section' => '_themename_footer_options',
         ));
+
+
+
+        $wp_customize->add_setting('_themename_footer_bg', array(
+            'default' => 'dark',
+            'sanitize_callback' => '_themename_sanitize_footer_bg',
+            'transport' => 'postMessage'
+        ));
+
+        $wp_customize->add_control('_themename_footer_bg', array(
+            'type' => 'select',
+            'label' => esc_html__( 'Footer Background', '_themename'),
+            'choices' => array(
+                'light' => esc_html__('Light', '_themename'),
+                'dark' => esc_html__('Dark', '_themename'),
+            ),
+            'section' => '_themename_footer_options'
+        ));
+
+
+        $wp_customize->add_setting('_themename_footer_layout', array(
+            'default' => '3,3,3,3',
+            'transport' => 'postMessage',
+            'sanitize_callback' => 'sanitize_text_field',
+            'validate_callback' => '_themename_validate_footer_layout'
+            
+        ));
+
+        $wp_customize->add_control('_themename_footer_layout', array(
+            'type' => 'text',
+            'label' => esc_html__( 'Footer Layout', '_themename'),
+            'section' => '_themename_footer_options',
+        ));
+
+
     }
+
+
     add_action( 'customize_register', '_themaname_customize_register');
 
     function _themename_sanitize_site_info($input){
@@ -30,7 +115,26 @@
             'title' => array()
         ));
         return wp_kses( $input, $allowed);
+    } 
+    
+    function _themename_sanitize_footer_bg($input){
+        $valid = array(
+            'light',
+            'dark'
+        );
+        if(in_array($input, $valid, true)){
+            return $input;
+        }
+        return 'dark';
     }   
+
+    function _themename_validate_footer_layout( $validity, $value) {
+        if(!preg_match('/^([1-9]|1[012])(,([1-9]|1[012]))*$/', $value)) {
+            $validity->add('invalid_footer_layout', esc_html__( 'Footer layout is invalid', '_themename' ));
+        }
+        return $validity;
+    }
+
 
 
 ?>
